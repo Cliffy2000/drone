28e2d5e758054f1e424f9d4be6c40e7fda3ade94
Cliff <cliffyyc@outlook.com>
2022-12-21 21:18:17 -0800

    pygame control functional

--- scripts/threading_test.py
+++ scripts/threading_test.py
@@ -4,36 +4,45 @@ import cv2
 import time
 
 
-drone = Tello()
-drone.connect()
-
-drone.streamon()
+# drone = Tello()
+# drone.connect()
+# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#
+# drone.streamon()
 
 
 def camera():
     while True:
         frame = drone.get_frame_read()
         f = frame.frame
+
+        gray = cv2.cvtColor(f, cv2.COLOR_BGR2GRAY)
+        # Detect faces
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        # Draw rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(f, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
         cv2.imshow("img", f)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            drone.land()
-            print(f'battery: {drone.get_battery()}')
+        if cv2.waitKey(1) & 0xFF == 27:
             break
 
 
 def control():
     time.sleep(5)
-    drone.takeoff()
-    time.sleep(1)
-    drone.land()
+    #drone.takeoff()
+    while True:
+        key = cv2.waitKey(1) & 0xff
+        if key == 27:  # ESC
+            break
+        elif key == ord('w'):
+            print('w')
+    #drone.land()
 
 
 if __name__ == "__main__":
-    try:
-        thread1 = threading.Thread(target=camera)
-        thread2 = threading.Thread(target=control)
-        thread1.start()
-        thread2.start()
-    finally:
-        drone.land()
+    thread1 = threading.Thread(target=camera)
+    thread2 = threading.Thread(target=control)
+    #thread1.start()
+    thread2.start()
